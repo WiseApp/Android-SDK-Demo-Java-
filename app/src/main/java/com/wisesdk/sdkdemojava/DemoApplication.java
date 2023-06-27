01b9c1df2c0e5309285c2e9089771b2c590d48d4
Ravi Tamada <ravi8x@gmail.com>
2023-06-27 13:17:47 +0530

    Updated demo to 1.1.4

--- app/src/main/java/com/wisesdk/sdkdemojava/DemoApplication.java
+++ app/src/main/java/com/wisesdk/sdkdemojava/DemoApplication.java
@@ -16,16 +16,12 @@ public class DemoApplication extends Application {
      * Initialise should be called as soon as app starts
      */
     private void initWiseSdk() {
-        WiseSDK.Companion.getInstance(this).initialize(new WiseSDK.WiseSDKInitListener() {
-            @Override
-            public void onVendorIdError() {
+        // set vendor Id and namespace
+        // Send an email to info@wiseapp.live to get your VENDOR_ID and NAMESPACE
+        // to be used in the Android Integration steps below
+        WiseSDK.Companion.getInstance(this).init("5f72e04f121699872486dc80", "wise");
 
-            }
-
-            @Override
-            public void onInitialised() {
-
-            }
-        });
+        // custom lens icon
+        WiseSDK.Companion.getInstance(this).setLensIcon(R.mipmap.ic_launcher);
     }
 }
--- app/src/main/java/com/wisesdk/sdkdemojava/MainActivity.java
+++ app/src/main/java/com/wisesdk/sdkdemojava/MainActivity.java
@@ -1,6 +1,7 @@
 package com.wisesdk.sdkdemojava;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -13,16 +14,7 @@ import com.wise.sdk.core.WiseSDK;
 public class MainActivity extends AppCompatActivity implements WiseSDK.WiseSDKMeetingListener {
     private String tag = MainActivity.class.getSimpleName();
 
-    /**
-     * Sample teacher and student tokens.
-     * Follow the wise API docs to fetch the required values
-     * https://www.notion.so/wiseapp/Wise-API-Documentation-a38e78c9a79d439ca7d13bae9d2d5582
-     */
-    private String teacherToken = "5f72e04f121699872486dc8012345678";
-    private String studentToken = "5fb8ab8cc91e3444349dc78165404171";
-
-    // classroom ID
-    private String classId = "5fb8ab8cc91e3444349dc781";
+    private String classPublicID = "649a8a5cbc098a31f0808bfc35114999";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,72 +22,21 @@ public class MainActivity extends AppCompatActivity implements WiseSDK.WiseSDKMe
         setContentView(R.layout.activity_main);
 
         /**
-         * Starting a new meeting
-         * Check for any existing zoom meeting before starting a new one
-         * */
-        findViewById(R.id.btn_start_meeting).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // goto meeting if it's in progress
-                if (WiseSDK.Companion.getInstance(MainActivity.this).isMeetingInProgress()) {
-                    WiseSDK.Companion.getInstance(MainActivity.this).returnToMeeting();
-                } else {
-                    // start meeting
-                    WiseSDK.Companion.getInstance(MainActivity.this).startMeeting(teacherToken, classId, MainActivity.this);
-                }
-            }
-        });
-
-        /**
-         * Starting a new meeting with meeting params
-         * Check for any existing zoom meeting before starting a new one
-         * */
-        findViewById(R.id.btn_start_meeting_with_params).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // goto meeting if it's in progress
-                if (WiseSDK.Companion.getInstance(MainActivity.this).isMeetingInProgress()) {
-                    WiseSDK.Companion.getInstance(MainActivity.this).returnToMeeting();
-                } else {
-                    // start meeting
-                    // Replace the below values you get from wise API
-                    // https://www.notion.so/wiseapp/Wise-API-Documentation-a38e78c9a79d439ca7d13bae9d2d5582
-                    WiseSDK.Companion.getInstance(MainActivity.this).startMeetingWithParams(
-                            classId,
-                            "https://wise-live.zoom.us/s/96979300288?zak=eyJ0eXAiOiJKV1QiLCJzdiI6IjAwMDAwMSIsInptX3NrbSI6InptX28ybSIsImFsZyI6IkhTMjU2In0.eyJhdWQiOiJjbGllbnRzbSIsInVpZCI6IlZsakdmNnpOU2FPRzR2d1p3M0ZhWnciLCJpc3MiOiJ3ZWIiLCJzayI6IjAiLCJzdHkiOjk5LCJ3Y2QiOiJhdzEiLCJjbHQiOjAsIm1udW0iOiI5Njk3OTMwMDI4OCIsImV4cCI6MTY1NDQ5MjMzMiwiaWF0IjoxNjQ2NzE2MzMyLCJhaWQiOiJtZV9CVjB3d1RkLWZmZ3ctakZGZml3IiwiY2lkIjoiIn0.It_dUDsnSI_zHkWuYIGNE_nP0XMtWY09It63p9DQPp4",
-                            "96979300288",
-                            "666080",
-                            "eyJ0eXAiOiJKV1QiLCJzdiI6IjAwMDAwMSIsInptX3NrbSI6InptX28ybSIsImFsZyI6IkhTMjU2In0.eyJhdWQiOiJjbGllbnRzbSIsInVpZCI6IlZsakdmNnpOU2FPRzR2d1p3M0ZhWnciLCJpc3MiOiJ3ZWIiLCJzayI6IjAiLCJzdHkiOjk5LCJ3Y2QiOiJhdzEiLCJjbHQiOjAsIm1udW0iOiI5Njk3OTMwMDI4OCIsImV4cCI6MTY1NDQ5MjMzMiwiaWF0IjoxNjQ2NzE2MzMyLCJhaWQiOiJtZV9CVjB3d1RkLWZmZ3ctakZGZml3IiwiY2lkIjoiIn0.It_dUDsnSI_zHkWuYIGNE_nP0XMtWY09It63p9DQPp4",
-                            "VljGf6zNSaOG4vwZw3FaZw",
-                            "169316985",
-                            "",
-                            true,
-                            MainActivity.this
-                    );
-                }
-            }
-        });
-
-        /**
          * Joining a live meeting
          * */
-        findViewById(R.id.btn_join_meeting).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // goto meeting if it's in progress
-                if (WiseSDK.Companion.getInstance(MainActivity.this).isMeetingInProgress()) {
-                    WiseSDK.Companion.getInstance(MainActivity.this).returnToMeeting();
-                } else {
-                    // join meeting
-                    WiseSDK.Companion.getInstance(MainActivity.this)
-                            .joinMeeting(
-                                    studentToken,
-                                    "",
-                                    "",
-                                    "Student Name",
-                                    MainActivity.this
-                            );
-                }
+        findViewById(R.id.btn_join_meeting).setOnClickListener(view -> {
+            // goto meeting if it's in progress
+            if (WiseSDK.Companion.getInstance(MainActivity.this).isMeetingInProgress()) {
+                WiseSDK.Companion.getInstance(MainActivity.this).returnToMeeting();
+            } else {
+                // join meeting
+                WiseSDK.Companion.getInstance(MainActivity.this)
+                        .joinMeeting(
+                                classPublicID,
+                                "3343222",
+                                "Mr John",
+                                MainActivity.this
+                        );
             }
         });
     }
@@ -106,18 +47,8 @@ public class MainActivity extends AppCompatActivity implements WiseSDK.WiseSDKMe
     }
 
     @Override
-    public void onMeetingEnded() {
-        Log.d(tag, "onMeetingEnded");
-    }
-
-    @Override
     public void onMeetingEndedByHost() {
         Log.d(tag, "onMeetingEndedByHost");
-    }
-
-    @Override
-    public void onMeetingEndedWithError(@NonNull String s) {
-        Log.d(tag, "onMeetingEndedWithError");
     }
 
     @Override
@@ -136,7 +67,27 @@ public class MainActivity extends AppCompatActivity implements WiseSDK.WiseSDKMe
     }
 
     @Override
-    public void onSDKError(int i) {
-        Log.d(tag, "onSDKError");
+    public void onInitialised() {
+
+    }
+
+    @Override
+    public void onMeetingEnded(@Nullable String s) {
+
+    }
+
+    @Override
+    public void onMeetingEndedWithError(@NonNull String s, int i, int i1) {
+
+    }
+
+    @Override
+    public void onSDKError(int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onVendorIdError() {
+
     }
 }
